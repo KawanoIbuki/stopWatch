9d810e20f89a6a4df154de407101b7d5cb07c6b0
Wasou / Wakana Hashimoto <48522543+xwasoux@users.noreply.github.com>
2021-02-14 15:15:46 +0900

    Update stopWatch.ino

--- stopWatch.ino
+++ stopWatch.ino
@@ -9,13 +9,7 @@ int Count = 0,startCheck = 0;
 void setup() {
   // put your setup code here, to run once:
   M5.begin();
-  //GPIO setting
-  //Button A = GPIO 39
-  //Button B = GPIO 38
-  //Button C = GPIO 37
-  pinMode(BUTTONA, INPUT);
-  pinMode(BUTTONB, INPUT);
-  pinMode(BUTTONC, INPUT);
+
   M5.Lcd.fillScreen(BLACK);
   M5.Lcd.setTextColor(WHITE);
   M5.Lcd.setTextSize(3);
@@ -23,15 +17,13 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-      M5.update();
-      if (M5.BtnA.wasPressed())
-  {
+  M5.update();
+  
+  if ( M5.BtnA.wasPressed() ){
     startCheck = 1;
   }
-  if(startCheck == 1)
-  {
+  if(startCheck == 1){
     M5.Lcd.printf("%d",Count);
-    //int %d float %f
     Count++;
     delay(1000);
     M5.Lcd.fillScreen(BLACK);
