c477d9d420aea1014c04840f021dbc260edb3385
Wasou / Wakana Hashimoto <48522543+xwasoux@users.noreply.github.com>
2021-02-17 03:49:06 +0900

    Update stopWatch.ino

--- stopWatch.ino
+++ stopWatch.ino
@@ -1,10 +1,6 @@
 #include <M5Stack.h>
-#define BUTTONA 39
-#define BUTTONB 38
-#define BUTTONC 37
-#define BUTTON_ON 0
-#define BUTTON_OFF 1
-int Count = 0,startCheck = 0;
+int counter = 0;
+//counter[0]=>ns, [1]=>ms, [2]=>s, [3]=>,min
 
 void setup() {
   // put your setup code here, to run once:
@@ -12,20 +8,19 @@ void setup() {
 
   M5.Lcd.fillScreen(BLACK);
   M5.Lcd.setTextColor(WHITE);
-  M5.Lcd.setTextSize(3);
+  M5.Lcd.setTextSize(5);
+  M5.Lcd.setCursor(10, 10);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   M5.update();
   
-  if ( M5.BtnA.wasPressed() ){
-    startCheck = 1;
-  }
-  if(startCheck == 1){
-    M5.Lcd.printf("%d",Count);
-    Count++;
-    delay(1000);
+  //stop watch start
+  while(M5.BtnA.wasPressed()){
+    M5.Lcd.printf("%d",counter);
+    
+    counter++;
     M5.Lcd.fillScreen(BLACK);
   }
 }
