6c7f7b33a35b4cd80d6769b8e8ef640e337fb287
Kaito <eh20a015@oecu.jp>
2021-02-18 10:28:43 +0900

    Update stopWatch.ino

--- stopWatch.ino
+++ stopWatch.ino
@@ -1,13 +1,24 @@
+#include <M5Stack.h>
+#define BUTTONA 39
+#define BUTTONB 38
 #define BUTTONC 37
 #define BUTTON_ON 0
 #define BUTTON_OFF 1
-int Count = 0,startCheck = 0;
 int secondCount = 0, minCount = 0, startCheck = 0, minRap = 0, secondRap = 0;
-
 void setup() {
   // put your setup code here, to run once:
-@@ -23,17 +23,28 @@ void setup() {
-
+  M5.begin();
+  //GPIO setting
+  //Button A = GPIO 39
+  //Button B = GPIO 38
+  //Button C = GPIO 37
+  pinMode(BUTTONA, INPUT);
+  pinMode(BUTTONB, INPUT);
+  pinMode(BUTTONC, INPUT);
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setTextColor(WHITE);
+  M5.Lcd.setTextSize(3);
+}
 void loop() {
   // put your main code here, to run repeatedly:
   M5.update();
@@ -22,22 +33,9 @@ void loop() {
         minCount++;
       }
       M5.update();
-      if (M5.BtnA.wasPressed())
-  {
-    startCheck = 1;
-  }
-  if(startCheck == 1)
-  {
-    M5.Lcd.printf("%d",Count);
-    //int %d float %f
-    Count++;
-    delay(1000);
-    M5.Lcd.fillScreen(BLACK);
       if (M5.BtnB.wasPressed()) {
-
         minRap = minCount;
         secondRap = secondCount;
-
       }
       M5.Lcd.setCursor(30, 30);
       M5.Lcd.printf("%d:%d", minRap, secondRap);
