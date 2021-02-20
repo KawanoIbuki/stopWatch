283c155d678faf162bdd515e8eb231d48d78f83d
xwasoux <oxwasouxo@gmail.con>
2021-02-20 22:00:48 +0900

    first commit

--- stopWatch.ino
+++ stopWatch.ino
@@ -1,74 +1,83 @@
 #include <M5Stack.h>
-#define BUTTONA 39
-#define BUTTONB 38
-#define BUTTONC 37
-#define BUTTON_ON 0
-#define BUTTON_OFF 1
-int secondCount = 0, minCount = 0, startCheck = 0, minRap = 0, secondRap = 0;
-bool Speedmetor = false,Raptimer = true;
+int secondCount =  minCount =  startCheck =  minRap =  secondRap = 0;
+
+//sd setting
+unsigned int auiSize = 0;
+unsigned int auiCnt = 0;
+
+void writeData(minRap, secondRap) {
+  file = SD.open(fname, FILE_APPEND);
+
+  file.println(timeStr + "," + minRap + "," + secondRap);
+
+  file.close
+}
+
 void setup() {
-  // put your setup code here, to run once:
   M5.begin();
-  //GPIO setting
-  //Button A = GPIO 39
-  //Button B = GPIO 38
-  //Button C = GPIO 37
-  pinMode(BUTTONA, INPUT);
-  pinMode(BUTTONB, INPUT);
-  pinMode(BUTTONC, INPUT);
+
   M5.Lcd.fillScreen(BLACK);
   M5.Lcd.setTextColor(WHITE);
   M5.Lcd.setTextSize(3);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   M5.update();
-  if(M5.BtnA.wasPressed() && !Raptimer){
+
+  if (M5.BtnA.wasPressed() && !Raptimer) {
     Raptimer = true;
     Speedmetor = false;
     M5.Lcd.fillScreen(BLACK);
   }
-  if(M5.BtnC.wasPressed() && !Speedmetor){
+
+  if (M5.BtnC.wasPressed() && !Speedmetor) {
     Raptimer = false;
     Speedmetor = true;
     M5.Lcd.fillScreen(BLACK);
   }
-  if(Speedmetor){
-    M5.Lcd.printf("Speedmetor");
-  }
-  if(Raptimer){
+
+  if (Speedmetor) M5.Lcd.printf("Speedmetor");
+
+  if (Raptimer) {
     M5.Lcd.setCursor(10, 10);
     M5.Lcd.printf("%d:%d", minCount, secondCount);
+
     M5.update();
+
     if (M5.BtnA.wasPressed() && Raptimer) {
-      while (Raptimer) {
+      while (1) {
         M5.Lcd.setCursor(10, 10);
         M5.Lcd.printf("%d:%d", minCount, secondCount);
-        //int %d float %f
         secondCount++;
+
         if (secondCount == 60) {
           secondCount = 0;
           minCount++;
-        }
+        } //end of if
+
         M5.update();
-        
+
         if (M5.BtnB.wasPressed()) {
-  
           minRap = minCount;
           secondRap = secondCount;
-  
-        }
+        } //end of if
+
         M5.Lcd.setCursor(30, 30);
         M5.Lcd.printf("%d:%d", minRap, secondRap);
-        //delay(1000);
         M5.Lcd.fillScreen(BLACK);
-        if(M5.BtnC.wasPressed() && !Speedmetor){
+
+        writeData(minRap, secondRap);
+
+        if (M5.BtnC.wasPressed() && !Speedmetor) {
           Raptimer = false;
           Speedmetor = true;
           M5.Lcd.fillScreen(BLACK);
-        }
-      }
-    }
-  }
-}
+        } //end of if (M5.BtnA.wasPressed() && Raptimer)
+
+      } //end of while
+
+    } //end of if
+
+  } //end of if
+
+} //end of loop()
