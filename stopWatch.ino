38270b6dfeb2a1a91eb19bc54b5ff2c2f8ac11ef
Kaito <eh20a015@oecu.jp>
2021-02-21 02:16:12 +0900

    ラップタイマー完成
    原型:0

--- stopWatch.ino
+++ stopWatch.ino
@@ -1,16 +1,18 @@
 #include <M5Stack.h>
 #include <Ticker.h>
 
-//timer
-Ticker ticker;
+Ticker tickerTime;
 
+//timer
+unsigned int msCount = 0;
 unsigned int secondCount = 0;
 unsigned int minCount = 0;
 unsigned int startCheck = 0;
 unsigned int minRap = 0;
 unsigned int secondRap = 0;
+unsigned int msRap = 0;
 unsigned int rapCount = 0;
-bool StartRapTimer = false;
+
 //sd setting
 unsigned int auiSize = 0;
 unsigned int auiCnt = 0;
@@ -18,40 +20,62 @@ unsigned int auiCnt = 0;
 File file;
 const char* fname = "/rapTime_log.csv";
 
-void writeData(int rapCount, int minRap, int secondRap) {
+void writeData(int rapCount, int minRap, int secondRap, int msRap) {
   file = SD.open(fname, FILE_APPEND);
-  file.println(String(rapCount) + "," + String(minRap) + "," + String(secondRap));
+  file.println(String(rapCount) + "," + String(minRap) + "," + String(secondRap) + "," + String(msRap));
   file.close();
 }
 
-void countupTimer() {
+void countUp() {
+  msCount++;
+}
+
+void rapTime() {
+  rapCount++;
+
+  minRap = (msCount / 60000) % 3600;
+  secondRap = (msCount / 1000) % 60;
+  msRap = msCount & 1000;
+
+  //write data to csv file
+  writeData(rapCount, minRap, secondRap, msRap);
+}
+void Timer() {
   M5.Lcd.fillScreen(BLACK);
-//timer mode
-  //init
-  M5.update();
-  //carry
-  if (secondCount == 60) {
-    secondCount = 0;
-    minCount++;
-  } //end of if
+  tickerTime.attach_ms(1, countUp);
 
-  //save rap time
-  M5.Lcd.setCursor(140, 200);
-  M5.Lcd.printf("RAP");
+  //timer mode
+  while (1) {
+    //init
+    M5.update();
+    M5.Lcd.fillScreen(BLACK);
 
-  //print time
-  M5.Lcd.setCursor(10, 10);
-  M5.Lcd.printf("%d:%d", minCount, secondCount);
-  // ticker.attach(1, printMillis);
+    //save rap time
+    M5.Lcd.setCursor(140, 200);
+    M5.Lcd.printf("RAP");
+    if (M5.BtnB.wasPressed()) {
+      rapTime();
+    } //end of if
 
-  //print rap time
-  M5.Lcd.setCursor(130, 130);
-  M5.Lcd.printf("%d - %d:%d", rapCount, minRap, secondRap);
+    //print time
+    M5.Lcd.setCursor(10, 10);
+    M5.Lcd.printf("Time: %2d:%2d.%3d", (msCount / 60000) % 3600, (msCount / 1000) % 60, msCount % 1000);
+    M5.Lcd.setCursor(10, 50);
+    M5.Lcd.printf("%10d", msCount);
 
-  //Quit (return to loop())
-  M5.Lcd.setCursor(230, 200);
-  M5.Lcd.printf("Quit");
-  secondCount++;
+    //print rap time
+    M5.Lcd.setCursor(40, 100);
+    M5.Lcd.printf("%d - %d:%d.%d", rapCount, minRap, secondRap, msRap);
+
+    //Quit (return to loop())
+    M5.Lcd.setCursor(220, 200);
+    M5.Lcd.printf("Quit");
+    if (M5.BtnC.wasPressed()) {
+      M5.Power.reset();
+      M5.Lcd.fillScreen(BLACK);
+      return;
+    }
+  } //end of while (1)
 }
 
 void setup() {
@@ -62,34 +86,19 @@ void setup() {
   M5.Lcd.setTextSize(3);
 
   file = SD.open(fname, FILE_APPEND);
-  file.println("number,min,second");
+  file.println("number,min,second,msRap");
   file.close();
 }
 
 void loop() {
   M5.update();
-  if(!StartRapTimer){
-    M5.Lcd.setCursor(10, 10);
-    M5.Lcd.printf("Press A button to start rap system");
-    M5.Lcd.setCursor(20, 200);
-    M5.Lcd.printf("START");
-  }
 
-  if (M5.BtnA.wasPressed()){
-    StartRapTimer = true;
-    ticker.attach(1,countupTimer);
-  }
-  
-  if (M5.BtnB.wasPressed()) {
-    minRap = minCount;
-    secondRap = secondCount;
-    rapCount++;
-    writeData(rapCount, minRap, secondRap);
-  } //end of if
-  
-  if (M5.BtnC.wasPressed()) {
-    M5.Lcd.fillScreen(BLACK);
-    StartRapTimer = false;
-  }
+  M5.Lcd.setCursor(10, 10);
+  M5.Lcd.printf("Press A button to start rap system");
+
+  //start timer
+  M5.Lcd.setCursor(20, 200);
+  M5.Lcd.printf("START");
+  if (M5.BtnA.wasPressed()) Timer();
 
 } //end of loop()
